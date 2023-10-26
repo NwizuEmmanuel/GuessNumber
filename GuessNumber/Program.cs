412f94dd9eebb9f79b206e619f2b03cc070d027e
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2023-10-26 22:25:57 +0800

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿class GuessNumber
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(1,10);
+
+        // ask user to guess the number
+        Console.Write("Guess the number? ");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+
+        // check if:
+        if (userInput == randomNumber)
+        {
+            Console.WriteLine("Correct!");
+        }else if (userInput < randomNumber) {
+            Console.WriteLine("Too small.");
+        }else
+        {
+            Console.WriteLine("Too big");
+        }
+
+        Console.WriteLine($"Number = {randomNumber}");
+    }
+}
